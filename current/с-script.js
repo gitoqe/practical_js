b293e2a76a31a955dcb15109f5e879256a7feb72
skrttem <53040407+gitoqe@users.noreply.github.com>
2019-07-22 17:51:23 +0300

    8 lesson - start video

--- current/с-script.js
+++ current/с-script.js
@@ -1,1 +1,6 @@
+//todo pre-8
 document.querySelector('p').innerHTML = 'bye';//получение параграфа
+//todo  8 Lesson
+document.querySelector('.onclick').onclick = function(){
+    console.log('click');
+}
