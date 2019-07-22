3ce0238d63baeefd016e44a2efd1ee9bf378238c
skrttem <53040407+gitoqe@users.noreply.github.com>
2019-07-22 13:20:23 +0300

    7 lesson. task 16/20
    task: 16/20

--- 7-array-cycles/script.js
+++ 7-array-cycles/script.js
@@ -1,15 +1,13 @@
 // 1. Выведите на экран числа от 0 до 100.
 let p_sto = document.querySelector('#sto');
 for (let i = 0; i <= 100; i++) {
-    //console.log(i); //вывод в консоль
     p_sto.innerHTML += i + ' ';
 }
 
 // 2. Вывести четные числа от 0 до 101.
 let p_chet = document.querySelector('#chet');
 for (let i = 0; i < 101; i++) {
-    if (i % 2 == 0){    //очень рад, что в js работает остаток от деления и инкремент
-        //console.log(i);
+    if (i % 2 == 0){
         p_chet.innerHTML += i + ' ';
     }
 }
@@ -17,7 +15,6 @@ for (let i = 0; i < 101; i++) {
 // 3. Вывести числа в следущей последовательности: 200 199 198 ... 0.
 let p_reverse = document.querySelector('#reverse');
 for (let i = 200; i >= 0; i--) {
-    //console.log(i);
     p_reverse.innerHTML += i + ' ';
 }
 
@@ -27,22 +24,20 @@ let sum_sto = 0;
 for (let i = 0; i <= 100; i++) {
     sum_sto += i;
 }
-console.log('Сумма чисел от 0 до 100 равна ' + sum_sto);
 p_sum_sto.innerHTML = 'Сумма чисел от 0 до 100 равна ' + sum_sto;
 
 // 5. Задайте два input. В первый вводится число, во второе его степень. С помощью цикла возведите число в степень. Напечатайте результат.
-function pow(){ // x^y
+function pow(){
     let x = document.querySelector('#base').value;
     let y = document.querySelector('#power').value;
     let otvet = 1;
     for (let i = 1; i <= y; i++) {
         otvet *= x;
     }
-    //console.log(otvet);
     document.querySelector('#pow').innerHTML = otvet;
 }
-document.querySelector('#base').oninput = pow;;
-document.querySelector('#power').oninput = pow;;
+document.querySelector('#base').oninput = pow;
+document.querySelector('#power').oninput = pow;
 
 /* 6. С помощью цикла напечатайте таблицу умножения на 7 от 1 до 9.
 Т.е.: 
@@ -63,33 +58,117 @@ for (let i = 1; i <= 50; i++) {
 }
 proizved.innerHTML = 'Произведение чисел от 1 до 50 равно ' + otvet_proizv;
 
-
 // 8. Выведите с помощью цикла коды спецсимволов от 1000 до 2000. Напомню, что спецсимвол получается комбинацией &#число; Например &#1222;
 let spec_code = document.querySelector('#spec-code');
 for (let i = 1000; i <= 2000; i++) {
     spec_code.innerHTML += '&#' + i + '; ';
 }
 
-
 // 9. Получите все параграфы внутри div.main страницы. Используя цикл пронумеруйте все параграфы. Номер добавьте внутри тега параграфа перед текстом.
+let main_array = document.querySelectorAll('.main p');
+for (let i = 0; i < main_array.length; i++) {
+    main_array[i].insertAdjacentHTML("afterbegin", i + 1 + '. ');
+}
+
+// 10. Создайте два поля. В одно пользователь вводит день рождения, во второй - месяц (в виде числа). Выведите знак зодиака под каким родился пользователь. Данные о знаке зодиака храните в массиве.
+let calend = [
+    // name, month-start, day-start, month-end, day-end
+    ['Овен ♈', 3, 21, 4, 19],
+    ['Телец ♉', 4, 20, 5, 20],
+    ['Близнецы ♊', 5, 21, 6, 20],
+    ['Рак ♋', 6, 21, 7, 22],
+    ['Лев ♌', 7, 23, 8, 22],
+    ['Дева ♍', 8, 23, 9, 22],
+    ['Весы ♎', 9, 23, 10, 22],
+    ['Скорпион ♏', 10, 23, 11, 21],
+    ['Стрелец ♐', 11, 22, 12, 21],
+    ['Козерог ♑', 12, 22, 1, 19],
+    ['Водолей ♒', 1, 20, 2, 18],
+    ['Рыбы ♓', 2, 19, 3, 20]
+]
+function zodiak(){
+    let D = document.querySelector('#day').value;
+    let M = document.querySelector('#month').value;
+    let znak = document.querySelector('#znak');
+    for (let i = 0; i < calend.length; i++) {
+        console.log(calend[i][0]);
+        if (((M == calend[i][1]) && (D >= calend[i][2])) || 
+            ((M == calend[i][3]) && (D <= calend[i][4]))){
+            znak.innerHTML = calend[i][0];
+        }
+    }
+}
+document.querySelector('#day').oninput = zodiak;
+document.querySelector('#month').oninput = zodiak;
+
+//11. С помощью JS найдите все span на странице и сделайте им цвет фона - желтый.
+let all_span = document.querySelectorAll('span');
+for (let i = 0; i < all_span.length; i++) {
+    all_span[i].style.backgroundColor = 'yellow';
+}
+
+//12. Выведите внизу страницы информацию о количестве параграфов.
+document.querySelector('#p-count').innerHTML = document.querySelectorAll('p').length;
+
+
+//13. Создайте массив состояний из единицы и 5 нулей. Т.е. [1, 0,0,0,0,0]. Выведите массив на страницу. Добавьте кнопку. При каждом нажатии кнопки смещайте единицу вправо на одну позицию и заново выводите массив в тот же блок что и раньше. Если единица достигла конца массива - не делайте ничего.
+let five_array = [1, 0, 0, 0, 0, 0];
+document.querySelector('#five').innerHTML = five_array;
+//let five_counter = 0;
+function fiveMove(){
+    //five_counter += 1;
+    if (five_array[5] != 1){ 
+        for (let i = 0; i < five_array.length; i++) {
+            if (five_array[i] == 1){
+                five_array[i] = 0;
+                five_array[i+1] = 1;
+                break;
+            }
+        }
+    }
+    document.querySelector('#five').innerHTML = five_array;
+}
+document.querySelector('#btn-five').onclick = fiveMove;
+
+// 14. Создайте массив с произвольными числами. Выведите количество элементов массива меньше нуля.
+let rand_array = [];
+let rand_count = Math.round(Math.random()*10);
+let negative = 0;
+for (let i = 0; i < rand_count; i++) {
+    rand_array[i] = Math.round(Math.random()*50 - 25);
+    if (rand_array[i] < 0){
+        negative++;
+    }
+}
+document.querySelector('#rand-array').innerHTML = rand_array;
+document.querySelector('#rand-count').innerHTML = rand_count;
+document.querySelector('#negative').innerHTML = negative;
+
+// 15. Создайте массив с произвольными числами. Выведите элементы массива меньше нуля.
+let array_menshe = [];
+let rand_count2 = Math.round(Math.random()*10);
+for (let i = 0; i < rand_count2; i++) {
+    array_menshe[i] = Math.round(Math.random()*50 - 25);
+    document.querySelector('#array-menshe').innerHTML += array_menshe[i] + ' ';
+    if (array_menshe[i] < 0){
+        document.querySelector('#lower-zero').innerHTML += array_menshe[i] + ' ';
+    }
+}
+
+// 16. Создайте input куда пользователь может ввести количество элементов массива. После нажатия кнопки создается массив заданной длинны заполненный единицами.
+let array_one = [];
+function setArr(){
+    let elem_num = document.querySelector('#elem-num').value;
+    for (let i = 0; i < elem_num; i++) {
+        array_one[i] = 1;
+    }
+    document.querySelector('#array-one').innerHTML = array_one;
+}
+document.querySelector('#btn-one').onclick = setArr;
+
+// 17. Создайте input куда пользователь может ввести количество элементов массива. После нажатия кнопки создается массив заданной длинны заполненный случайными числами от 0 до 100.
 
 /*
-10. Создайте два поля. В одно пользователь вводит день рождения, во второй - месяц (в виде числа). Выведите знак зодиака под каким родился пользователь. Данные о знаке зодиака храните в массиве.
-
-11. С помощью JS найдите все span на странице и сделайте им цвет фона - желтый.
-
-12. Выведите внизу страницы информацию о количестве параграфов.
-
-13. Создайте массив состояний из единицы и 5 нулей. Т.е. [1, 0,0,0,0,0]. Выведите массив на страницу. Добавьте кнопку. При каждом нажатии кнопки смещайте единицу вправо на одну позицию и заново выводите массив в тот же блок что и раньше. Если единица достигла конца массива - не делайте ничего.
-
-14. Создайте массив с произвольными числами. Выведите количество элементов массива меньше нуля.
-
-15. Создайте массив с произвольными числами. Выведите элементы массива меньше нуля.
-
-16. Создайте input куда пользователь может ввести количество элементов массива. После нажатия кнопки создается массив заданной длинны заполненный единицами.
-
-17. Создайте input куда пользователь может ввести количество элементов массива. После нажатия кнопки создается массив заданной длинны заполненный случайными числами от 0 до 100.
-
 18. Создайте массив заполненный числами и строками. Используя цикл создайте другой массив куда поместите только числа из первого массива.
 
 19. Создайте массив. Выведите максимальное значение из этого массива.
@@ -124,3 +203,9 @@ else {
 }
 }
 */
+
+// нумерация заданий
+let task_number = document.querySelectorAll('h3');
+for (let i = 0; i < task_number.length; i++) {
+    task_number[i].insertAdjacentHTML("beforeend", ' ' + (i + 1) + '.');
+}
